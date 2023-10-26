0aa607551830beed3a796419997117b66873f441
JVPpolitecnics <jvickeryp2223@gmail.com>
2023-10-26 20:23:45 +0200

    sll

--- bd.php
+++ bd.php
@@ -31,4 +31,18 @@ $sentencia->bindParam(':contrasenya', $contrasenya);
 $sentencia->execute();
 $conexion = closeBD();
 }
+function insertBird($nom_llati, $nom_comu, $cognom2, $id_ordre, $img){
+  $conexion = openBD();
+  
+  $sentenciaTxt = "insert into birdwatcher (nom, cognom1, cognom2, correu_electronic, contrasenya) values (:nom, :cognom1, :cognom2, :correu_electronic, :contrasenya)";
+  $sentencia = $conexion->prepare($sentenciaTxt);
+  $sentencia->bindParam(':nom', $nom);
+  $sentencia->bindParam(':cognom1', $cognom1);
+  $sentencia->bindParam(':cognom2', $cognom2);
+  $sentencia->bindParam(':correu_electronic', $correuElectronic);
+  $sentencia->bindParam(':contrasenya', $contrasenya);
+  
+  $sentencia->execute();
+  $conexion = closeBD();
+  }
 ?>
